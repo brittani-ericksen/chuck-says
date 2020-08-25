d12f66ec41ce650e571de941cf6c3d7839fbca9d
Brittani Ericksen <heybrittani@gmail.com>
2020-08-25 16:03:52 -0400

    Added modal overlay

--- js/index.js
+++ js/index.js
@@ -3,29 +3,62 @@
 const chuckSays = document.getElementById("chuckSays");
 const refreshQuote = document.getElementById("refreshQuote");
 const submitFormButton = document.getElementById("submitForm");
-const defaultCategory = "dev";
+const modalOverlay = document.querySelector(".modal-overlay");
+const modalCloseButton = document.getElementById("closeModal");
+let category = "dev";
 
 // function getQuote(category) {}
-const getQuote = (category) => {
+const getQuote = () => {
     const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
     
     get(url).then(function(fetchResponse) {
         chuckSays.innerHTML = fetchResponse.value;
+        modalOverlay.classList.toggle("open");
+    });
+}
+
+const getCategories = () => {
+    const url =`https://api.chucknorris.io/jokes/categories`;
+    const dropdownMenu = document.getElementById("categoryInput");
+
+    get(url).then(function(categoryArray) {
+        categoryArray.map(function(category) {
+            if (category !== "explicit") {
+                const categoryOption = document.createElement("option");
+                categoryOption.value = category;
+                categoryOption.text = category;
+                dropdownMenu.append(categoryOption);
+            }
+        });
     });
 }
 
 refreshQuote.addEventListener("click", function(e) {
     e.preventDefault();
-    getQuote(defaultCategory);
+    getQuote();
 });
 
-submitFormButton.addEventListener("click", function(e) {
+// submitFormButton.addEventListener("click", function(e) {
+//     e.preventDefault();
+//     const userInput = document.getElementById("categoryInput");
+//     const category = userInput.value;
+//     getQuote(category);
+// });
+
+const getChuckQuotes = document.getElementById("getChuckQuotes");
+
+getChuckQuotes.addEventListener('submit', e => {
     e.preventDefault();
     const userInput = document.getElementById("categoryInput");
-    const category = userInput.value;
-    getQuote(category);
+    category = userInput.value;
+    getQuote();
+});
+
+modalCloseButton.addEventListener('click', function(e) {
+    modalOverlay.classList.toggle("open");
 });
 
 (function() {
-    getQuote(defaultCategory)
+    getCategories();
+    getQuote();
 })();
